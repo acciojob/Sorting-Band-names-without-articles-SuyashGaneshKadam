603ec27c9c7ebabd2dcadb1f89a7699963c2efcc
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-22 01:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
 //your code here
+let ul = document.getElementById("band");
+let arr = ["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
+// console.log(arr);
 
+function getArticleLessString(input) {
+  let words = input.split(" ");
+  let nonArticleWords = words.filter((word) => {
+    let smallCaseWord = word.toLowerCase();
+    if (
+      smallCaseWord === "the" ||
+      smallCaseWord === "an" ||
+      smallCaseWord === "a"
+    ) {
+      return false;
+    }
+    return true;
+  });
+  return nonArticleWords.join(" ");
+}
+
+let hashMap = {};
+let arr1 = [];
+
+for (let i = 0; i < arr.length; i++) {
+  let articleLessString = getArticleLessString(arr[i]);
+  hashMap[articleLessString] = arr[i];
+  arr1.push(articleLessString);
+}
+arr1.sort();
+let outputList = [];
+arr1.forEach((newString) => {
+  outputList.push(hashMap[newString]); //
+});
+// console.log(outputList);
+
+for (let i = 0; i < outputList.length; i++) {
+  let li = document.createElement("li");
+  li.innerText = outputList[i];
+  ul.append(li);
+}
